4fb896ac0858d76d78c249402d250b4c1844f67d
Aaron Renner <aaron@animascodelabs.com>
2015-01-12 15:49:10 -0700

    Added the number of contacts

--- app/controllers/contacts/index.js
+++ app/controllers/contacts/index.js
@@ -1,5 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
-  sortProperties: ['lastName', 'firstName']
+  sortProperties: ['lastName', 'firstName'],
+
+  contactsCount: function(){
+    return this.get('model.length');
+  }.property('@each')
 });

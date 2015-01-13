11ad3b4603eb0bf2d2a90d59ae13b38e639385e7
Aaron Renner <aaron@animascodelabs.com>
2015-01-12 19:19:30 -0700

    Remove the contact if it's not saved on the new route

--- app/routes/contacts/new.js
+++ app/routes/contacts/new.js
@@ -8,5 +8,15 @@ export default Ember.Route.extend({
     this.render('contact.edit', {
       controller: 'contacts.new'
     });
+  },
+  actions:{
+    willTransition: function(transition){
+      var contact = this.controller.get('model');
+      if (contact.get('isNew')){
+        contact.deleteRecord();
+      }
+
+      return true;
+    }
   }
 });
